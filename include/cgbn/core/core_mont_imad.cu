f42257565f18b17677e581615aacd14f0b6f88cf
Seth Troisi <sethtroisi@google.com>
2021-11-10 15:39:50 -0800

    Use x[LIMBS] instead of x1 in mont_mul_imad

--- include/cgbn/core/core_mont_imad.cu
+++ include/cgbn/core/core_mont_imad.cu
@@ -27,28 +27,27 @@ namespace cgbn {
 template<class env>
 __device__ __forceinline__ void core_t<env>::mont_mul(uint32_t r[LIMBS], const uint32_t a[LIMBS], const uint32_t b[LIMBS], const uint32_t n[LIMBS], const uint32_t np0) {
   uint32_t sync=sync_mask(), group_thread=threadIdx.x & TPI-1;
-  uint32_t x[LIMBS], x1=0, x2, t, q, c;
-  
-  mpzero<LIMBS>(x);
-  
+  uint32_t x[LIMBS+1], x1, t, q, c;
+
+  mpzero<LIMBS+1>(x);
+
   #pragma nounroll
   for(int32_t thread=0;thread<TPI;thread++) {
     #pragma unroll
     for(int word=0;word<LIMBS;word++) {
       t=__shfl_sync(sync, b[word], thread, TPI);
-      
+
       chain_t<LIMBS+1> chain1;
       #pragma unroll
       for(int32_t index=0;index<LIMBS;index++)
         x[index]=chain1.madlo(a[index], t, x[index]);
-      x1=chain1.add(x1, 0);
-      
+      x[LIMBS]=chain1.add(x[limbs], 0);
+
       chain_t<LIMBS+1> chain2;
-      for(int32_t index=0;index<LIMBS-1;index++)
+      for(int32_t index=0;index<LIMBS;index++)
         x[index+1]=chain2.madhi(a[index], t, x[index+1]);
-      x1=chain2.madhi(a[LIMBS-1], t, x1);
-      x2=chain2.add(0, 0);
-      
+      x1=chain2.add(0, 0);
+
       q=__shfl_sync(sync, x[0], 0, TPI)*np0;
 
       chain_t<LIMBS+2> chain3;
@@ -56,23 +55,23 @@ __device__ __forceinline__ void core_t<env>::mont_mul(uint32_t r[LIMBS], const u
       for(int32_t index=0;index<LIMBS;index++)
         x[index]=chain3.madlo(n[index], q, x[index]);
       t=__shfl_sync(sync, x[0], threadIdx.x+1, TPI);
-      x1=chain3.add(x1, t);
-      x2=chain3.add(x2, 0);
-      
+      x[LIMBS]=chain3.add(x[LIMBS], t);
+      x1=chain3.add(x1, 0);
+
       chain_t<LIMBS+1> chain4;
       for(int32_t index=0;index<LIMBS-1;index++)
         x[index]=chain4.madhi(n[index], q, x[index+1]);
-      x[LIMBS-1]=chain4.madhi(n[LIMBS-1], q, x1);
-      x1=chain4.add(x2, 0);
+      x[LIMBS-1]=chain4.madhi(n[LIMBS-1], q, x[LIMBS]);
+      x[LIMBS]=chain4.add(x1, 0);
     }
   }
-  
+
   // r1:r0 <= 0x00000002 0xFFFFFFFD
-  t=__shfl_up_sync(sync, x1, 1, TPI);
-  
+  t=__shfl_up_sync(sync, x[LIMBS], 1, TPI);
+
   // all but most significant thread clears r1
   if(group_thread!=TPI-1)
-    x1=0;
+    x[LIMBS]=0;
   if(group_thread==0)
     t=0;
 
@@ -81,7 +80,7 @@ __device__ __forceinline__ void core_t<env>::mont_mul(uint32_t r[LIMBS], const u
   #pragma unroll
   for(int32_t index=1;index<LIMBS;index++)
     r[index]=chain5.add(x[index], 0);
-  c=chain5.add(x1, 0);
+  c=chain5.add(x[LIMBS], 0);
 
   c=-fast_propagate_add(c, r);
 
@@ -102,9 +101,9 @@ template<class env>
 __device__ __forceinline__ void core_t<env>::mont_reduce_wide(uint32_t r[LIMBS], const uint32_t lo[LIMBS], const uint32_t hi[LIMBS], const uint32_t n[LIMBS], const uint32_t np0, const bool zero) {
   uint32_t sync=sync_mask(), group_thread=threadIdx.x & TPI-1;
   uint32_t c, x[LIMBS], t, q, top;
-  
+
   mpset<LIMBS>(x, lo);
-  
+
   c=0;
   #pragma nounroll
   for(int32_t thread=0;thread<TPI;thread++) {
@@ -119,11 +118,11 @@ __device__ __forceinline__ void core_t<env>::mont_reduce_wide(uint32_t r[LIMBS],
 
       if(!zero)
         top=__shfl_sync(sync, hi[word], thread, TPI);
-    
+
       // shift right by 32 bits (top thread gets zero)
       t=__shfl_sync(sync, x[0], threadIdx.x+1, TPI);
       if(!zero) {
-        top=__shfl_sync(sync, hi[word], thread, TPI);    
+        top=__shfl_sync(sync, hi[word], thread, TPI);
         t=(group_thread==TPI-1) ? top : t;
       }
 
@@ -132,14 +131,14 @@ __device__ __forceinline__ void core_t<env>::mont_reduce_wide(uint32_t r[LIMBS],
         x[index]=chain2.madhi(n[index], q, x[index+1]);
       x[LIMBS-1]=chain2.madhi(n[LIMBS-1], q, c);
       c=chain2.add(0, 0);
-      
+
       x[LIMBS-1]=add_cc(x[LIMBS-1], t);
       c=addc(c, 0);
     }
   }
-  
+
   c=fast_propagate_add(c, x);
-  
+
   if(!zero && c!=0) {
     t=n[0]-(group_thread==0);   // n must be odd, so there is no chance for a carry ripple
 
